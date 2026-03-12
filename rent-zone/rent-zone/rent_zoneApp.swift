1fc758f69d2f4ae3c3e6b894a5e3f16d2e1e81e9
Aditya Bansal <adityabansalsafe@gmail.com>
2026-03-12 14:12:22 +0530

    Init repo

---
+++ rent-zone/rent-zone/rent_zoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  rent_zoneApp.swift
+//  rent-zone
+//
+//  Created by Aditya Bansal on 12/03/26.
+//
+
+import SwiftUI
+
+@main
+struct rent_zoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
