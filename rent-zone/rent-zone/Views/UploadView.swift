3e111464e2530d4c7a35748a3fd5ddbc992fc8fe
VANSH BANSAL <vanshbansal0210@gmail.com>
2026-03-13 10:45:46 +0530

    Created Upload View File

---
+++ rent-zone/rent-zone/Views/UploadView.swift
@@ -0,0 +1,18 @@
+//
+//  UploadView.swift
+//  rent-zone
+//
+//  Created by Vansh     on 13/03/26.
+//
+
+import SwiftUI
+
+struct UploadView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#Preview {
+    UploadView()
+}
