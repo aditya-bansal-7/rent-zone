24728df2bf699755ffb6e837e4673437752e8bf5
Payal Singh <payalsingh1031@gmail.com>
2026-03-13 10:18:26 +0530

    Develop home screen

---
+++ rent-zone/rent-zone/Home.swift
@@ -0,0 +1,18 @@
+//
+//  Home.swift
+//  rent-zone
+//
+//  Created by Payal Singh on 13/03/26.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Home()
+}
