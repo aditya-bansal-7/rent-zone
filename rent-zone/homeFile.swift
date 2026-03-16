661e6e60372781aaa15ed9c3a84988056afbaeff
Payal Singh <payalsingh1031@gmail.com>
2026-03-16 10:47:30 +0530

    home page view edit

---
+++ rent-zone/homeFile.swift
@@ -0,0 +1,18 @@
+//
+//  homeFile.swift
+//  rent-zone
+//
+//  Created by Payal Singh on 16/03/26.
+//
+
+import SwiftUI
+
+struct homeFile: View {
+    var body: some View {
+        Text("HOME PAGE")
+    }
+}
+
+#Preview {
+    homeFile()
+}
